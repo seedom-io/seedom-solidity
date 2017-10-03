7cfeb3726127e93465146d8d9e3c7a92c8d3625e
Alex Groleau <agroleau@teampalmtree.com>
2017-10-02 20:26:01 -0400

    removed ethpm, switched to npm, using truffle JS testing for unit and integration across the board, moved to node 8 async/await to clean up tests

--- contracts/Charity.sol
+++ contracts/Charity.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.4;
 
-import 'zeppelin/contracts/ownership/Ownable.sol';
-import 'zeppelin/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract Charity is Ownable {
     using SafeMath for uint256;
--- test/test_charity.js
+++ test/test_charity.js
@@ -1,3 +1,5 @@
+const assertJump = require('zeppelin-solidity/test/helpers/assertJump');
+
 var Charity = artifacts.require("./charity.sol");
 
 function timestamp(date) {
@@ -6,17 +8,13 @@ function timestamp(date) {
 
 contract('Charity', function(accounts) {
 
-  it("should set the owner to us", function() {
-    return Charity.deployed().then(function(instance) {
-      return instance.owner.call();
-    }).then(function(owner) {
-      assert.equal(owner, accounts[0], "Owner wasn't us");
-    });
+  it("should set the owner to us", async function() {
+    var instance = await Charity.deployed();
+    var actualOwner = await instance.owner.call();
+    assert.equal(actualOwner, accounts[0], "Owner wasn't us");
   });
 
-  it("should start properly", function() {
-
-    var that;
+  it("should start properly", async function() {
 
     var owner = accounts[0];
     var charity = accounts[1];
@@ -36,11 +34,76 @@ contract('Charity', function(accounts) {
     var revealTime = timestamp(revealDate);
     var endTime = timestamp(endDate);
 
-    return Charity.deployed().then(function(instance) {
+    var instance = await Charity.deployed();
 
-      that = instance;
+    var actualWinner = await instance.winner.call({from: owner});
+    assert.equal(0, actualWinner, "winner zero");
+    var actualCancelled = await instance.cancelled.call({from: owner});
+    assert.isOk(actualCancelled, "initially cancelled");
+    var actualTotalEntries = await instance.totalEntries.call({from: owner});
+    assert.equal(0, actualTotalEntries, "total entries zero");
+    var actualTotalRevealed = await instance.totalRevealed.call({from: owner});
+    assert.equal(0, actualTotalRevealed, "total revealed zero");
+    var actualTotalParticipants = await instance.totalParticipants.call({from: owner});
+    assert.equal(0, actualTotalParticipants, "total participants zero");
+    var actualTotalRevealers = await instance.totalRevealers.call({from: owner});
+    assert.equal(0, actualTotalRevealers, "total revealers zero");
+    
+    await instance.start(
+      charity,
+      charitySplit,
+      winnerSplit,
+      ownerSplit,
+      weiPerEntry,
+      startTime,
+      revealTime,
+      endTime,
+      {from: owner}
+    );
 
-      return instance.start(
+    var actualCharity = await instance.charity.call({from: owner});
+    assert.equal(charity, actualCharity, "charity does not match");
+    var actualCharitySplit = await instance.charitySplit.call({from: owner});
+    assert.equal(charitySplit, actualCharitySplit, "charity split does not match");
+    var actualWinnerSplit = await instance.winnerSplit.call({from: owner});
+    assert.equal(winnerSplit, actualWinnerSplit, "winner split does not match");
+    var actualOwnerSplit = await instance.ownerSplit.call({from: owner});
+    assert.equal(ownerSplit, actualOwnerSplit, "owner split does not match");
+    var actualWeiPerEntry = await instance.weiPerEntry.call({from: owner});
+    assert.equal(weiPerEntry, actualWeiPerEntry, "wei per entry does not match");
+    var actualStartTime = await instance.startTime.call({from: owner});
+    assert.equal(startTime, actualStartTime, "start time does not match");
+    var actualRevealTime = await instance.revealTime.call({from: owner});
+    assert.equal(revealTime, actualRevealTime, "reveal time does not match");
+    var actualEndTime = await instance.endTime.call({from: owner});
+    assert.equal(endTime, actualEndTime, "end time does not match");
+
+  });
+
+  it("should fail to start with zero charity", async function() {
+
+    var owner = accounts[0];
+    var charity = 0;
+    var charitySplit = 49;
+    var winnerSplit = 49;
+    var ownerSplit = 2;
+    var weiPerEntry = 1000;
+    
+    var startDate = new Date();
+    startDate.setSeconds(startDate.getSeconds() + 5);
+    var revealDate = new Date();
+    revealDate.setSeconds(startDate.getSeconds() + 5);
+    var endDate = new Date();
+    endDate.setSeconds(revealDate.getSeconds() + 5);
+
+    var startTime = timestamp(startDate);
+    var revealTime = timestamp(revealDate);
+    var endTime = timestamp(endDate);
+
+    var instance = await Charity.deployed();
+      
+    try {
+      await instance.start(
         charity,
         charitySplit,
         winnerSplit,
@@ -51,39 +114,10 @@ contract('Charity', function(accounts) {
         endTime,
         {from: owner}
       );
-      
-    }).then(function() {
-      return that.charity.call({from: owner});
-    }).then(function(actualCharity) {
-      assert.equal(charity, actualCharity, "charity does not match");
-    }).then(function() {
-      return that.charitySplit.call({from: owner});
-    }).then(function(actualCharitySplit) {
-      assert.equal(charitySplit, actualCharitySplit, "charity split does not match");
-    }).then(function() {
-      return that.winnerSplit.call({from: owner});
-    }).then(function(actualWinnerSplit) {
-      assert.equal(winnerSplit, actualWinnerSplit, "winner split does not match");
-    }).then(function() {
-      return that.ownerSplit.call({from: owner});
-    }).then(function(actualOwnerSplit) {
-      assert.equal(ownerSplit, actualOwnerSplit, "owner split does not match");
-    }).then(function() {
-      return that.weiPerEntry.call({from: owner});
-    }).then(function(actualWeiPerEntry) {
-      assert.equal(weiPerEntry, actualWeiPerEntry, "wei per entry does not match");
-    }).then(function() {
-      return that.startTime.call({from: owner});
-    }).then(function(actualStartTime) {
-      assert.equal(startTime, actualStartTime, "start time does not match");
-    }).then(function() {
-      return that.revealTime.call({from: owner});
-    }).then(function(actualRevealTime) {
-      assert.equal(revealTime, actualRevealTime, "reveal time does not match");
-    }).then(function() {
-      return that.endTime.call({from: owner});
-    }).then(function(actualEndTime) {
-      assert.equal(endTime, actualEndTime, "end time does not match");
-    });
+      assert.fail();
+    } catch (error) {
+      assertJump(error);
+    }
   });
+  
 });
