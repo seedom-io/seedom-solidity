160c4e3a6a1e85043bb457415a8a4286bef9d2af
Alex <agroleau@teampalmtree.com>
2018-02-18 01:26:49 -0700

    more voter script fixes

--- script/voter.js
+++ script/voter.js
@@ -2,8 +2,7 @@ const h = require('./helper');
 
 module.exports.options = [
     ['from', true],
-    ['charityNames', true],
-    ['charityAddresses', true],
+    ['charityNameStrings', true],
     ['endTime', true]
 ];
 
@@ -11,6 +10,13 @@ module.exports.run = async (state) => {
 
     const { env } = state;
 
+    env.charityNames = [];
+    env.charityAddresses = [];
+    for (const charityNameString of env.charityNameStrings) {
+        env.charityNames.push(h.hexRandom(charityNameString));
+        env.charityAddresses.push("0x0003dFf23dD77e4D74730d7DA3A0a672A5140D34");
+    }
+    
     const seedom = await state.interfaces.seedom;
     const seedomContractAddress = seedom.receipt.contractAddress;
     const seeder = await state.interfaces.seeder;
@@ -18,7 +24,8 @@ module.exports.run = async (state) => {
     env.seederReceipt = await seeder.deploy({
         charityNames: env.charityNames,
         charityAddresses: env.charityAddresses,
-        endTime: env.endTime
+        endTime: env.endTime,
+        seedomAddress: seedom.receipt.contractAddress
     }, {
         from: env.from
     });
